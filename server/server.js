e585049230235317bce7e355c64e18cab43b8998
Kevin <a01056539944@gmail.com>
2023-02-28 11:06:06 +0900

    a

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const http = require('http');
-const { Server } = require('socket.io');
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 const app = express();
-const { addUser, getUser, getUsersInRoom } = require('./users.js')
+const { addUser, getUser, getUsersInRoom } = require("./users.js");
 
-require('dotenv').config();
+require("dotenv").config();
 
 app.use(cors());
 
@@ -14,50 +14,61 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["*"],
   },
 });
 
-io.on('connection', socket => {
+io.on("connection", (socket) => {
   console.log("new connection !!");
 
-  socket.on('join_room', (username, room) => {
-
-    // say user joined
-    socket.emit('message', {
-      user: 'admin',
-      text: `${username} has joined the room ${room}`
-    })
-
-    socket.broadcast.to(room).emit('message', {
-      user: 'admin',
-      text: `${username} has joined the room ${room}`
-    })
+  socket.on("join_room", (username, room) => {
+    console.log("just join room " + room);
 
     socket.join(room);
-  })
 
-  socket.on('generate_game', (room, matrix) => {
-    socket.broadcast.to(room).emit('get_game', matrix);
-    console.log("????????????? " + room, matrix);
-  })
+    // say user joined
+    socket.emit("message", {
+      user: "admin",
+      text: `${username} has joined the room ${room}`,
+    });
 
-  socket.on('generate_alphabet_ game', (room, alphabet) => {
-    socket.broadcast.to(room).emit('get_1game', alphabet);
+    socket.broadcast.to(room).emit("message", {
+      user: "admin",
+      text: `${username} has joined the room ${room}`,
+    });
+  });
+
+  /**
+   * 게임생성
+   */
+  socket.on("generate_game", (room, matrix) => {
+    // socket.broadcast.to(room).emit("get_game", matrix);
+
+    console.log(`join room : ${room}`, matrix);
+
+    // io.emit("generate_game", "everyone"); // short form
+
+    io.to(room).emit("generate_game", matrix);
+  });
+
+  socket.on("generate_alphabet_ game", (room, alphabet) => {
+    socket.broadcast.to(room).emit("get_1game", alphabet);
     console.log("????????????? " + room, alphabet);
-  })
+  });
 
-  socket.on('send_message', (data) => { 
+  socket.on("send_message", (data) => {
     io.to(data.room).emit("receive_message", data);
-  })
+  });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     console.log(socket.id, " is disconnected");
   });
-});
 
+  socket.on("disconnect", () => {
+    console.log("Leave Socket");
+  });
+});
 
 server.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
-
